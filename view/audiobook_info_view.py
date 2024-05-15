e8865bad52af31f7ed6c7fe56d14b80c015d243a
Roman <ludoviksoso@gmail.com>
2024-05-16 03:15:50 +0700

    small ui improvements

--- view/audiobook_info_view.py
+++ view/audiobook_info_view.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView, QLabel, QPushButton, QTableWidget, QHeaderView, QScrollArea, QHBoxLayout, QGridLayout, QSpacerItem, QSizePolicy
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView, QLabel, QPushButton, QTableWidget, QHeaderView, QScrollArea, QHBoxLayout, QGridLayout, QSpacerItem, QSizePolicy, QSplitter
 
 class BookInfoTab(QWidget):
     def __init__(self):
@@ -7,24 +8,45 @@ class BookInfoTab(QWidget):
 
     def setup_ui(self):
         main_layout = QVBoxLayout()
-        self.scroll_area = QScrollArea()
-        self.scroll_area_widget_contents = QWidget()
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setWidget(self.scroll_area_widget_contents)
 
-        # Отдельный макет для содержимого scroll_area
-        content_layout = QVBoxLayout(self.scroll_area_widget_contents)
-        self.setup_book_info_tab(content_layout)
+        # Разделитель для распределения пространства
+        splitter = QSplitter()
+        splitter.setOrientation(Qt.Orientation.Vertical)
 
-        # Добавление scroll_area в основной макет
-        main_layout.addWidget(self.scroll_area)
+        # Область прокрутки для информации о книге
+        self.book_info_scroll_area = QScrollArea()
+        self.book_info_scroll_area_widget_contents = QWidget()
+        self.book_info_scroll_area.setWidgetResizable(True)
+        self.book_info_scroll_area.setWidget(self.book_info_scroll_area_widget_contents)
+
+        book_info_layout = QVBoxLayout(self.book_info_scroll_area_widget_contents)
+        self.setup_book_info_tab(book_info_layout)
+
+        # Область прокрутки для кнопок и таблицы файлов
+        self.action_scroll_area = QScrollArea()
+        self.action_scroll_area_widget_contents = QWidget()
+        self.action_scroll_area.setWidgetResizable(True)
+        self.action_scroll_area.setWidget(self.action_scroll_area_widget_contents)
+
+        action_layout = QVBoxLayout(self.action_scroll_area_widget_contents)
+        self.setup_action_buttons(action_layout)
+        self.setup_file_table(action_layout)
+
+        # Добавление областей прокрутки в разделитель
+        splitter.addWidget(self.book_info_scroll_area)
+        splitter.addWidget(self.action_scroll_area)
+
+        # Установка начальных размеров областей
+        splitter.setStretchFactor(0, 7)  # 70% для информации о книге
+        splitter.setStretchFactor(1, 3)  # 30% для кнопок и таблицы файлов
+
+        # Добавление разделителя в основной макет
+        main_layout.addWidget(splitter)
         self.setLayout(main_layout)
 
     def setup_book_info_tab(self, layout):
         self.setup_image_view(layout)
         self.setup_info_labels(layout)
-        self.setup_action_buttons(layout)
-        self.setup_file_table(layout)
 
         # Добавим пружину в конец layout
         spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
